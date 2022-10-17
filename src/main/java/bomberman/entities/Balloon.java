5f59bfe8390ebd8ab4ea1ad5d90b54d9846ac015
[firstname lastname] <86699994+ngdung2511@users.noreply.github.com>
2022-10-17 22:47:30 +0700

    4 directions bug

--- src/main/java/bomberman/entities/Balloon.java
+++ src/main/java/bomberman/entities/Balloon.java
@@ -3,6 +3,7 @@ package bomberman.entities;
 import bomberman.graphics.Sprite;
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,18 +13,43 @@ public class Balloon extends Entity {
     public static int ANIMATE = 30;
     public static int TIME = 10;
     public static final int VELOCITY = 1;
-    public static final int DIRECTIONS = 2;
+    public static final int DIRECTIONS = 4;
 
+    public List<java.lang.Integer> listDirection = new ArrayList<java.lang.Integer>();
+
+    public void addDirection() {
+        for (int i = 0; i < DIRECTIONS; i++) {
+            listDirection.add(i);
+        }
+    }
+
+    public boolean isHitWall = false;
+    private static final int MAXWIDTHBLOOM = 28;
+    private static final int MAXHEIGHTBLOOM = 30;
     public Balloon(int x, int y, Image img) {
         super(x, y, img);
     }
 
+    public static List get(List numbers, int noOfRandomNumbers) {
+        List list = new ArrayList(numbers);
+        List randomList = new ArrayList();
+
+        Random rd = new Random();
+        for (int i = 0; i < noOfRandomNumbers; i++) {
+            int index = (int) (rd.nextDouble() * list.size());
+            randomList.add(list.get(index));
+            list.remove(index);
+        }
+        return randomList;
+    }
     public void moveRight() {
         x+=VELOCITY;
-        System.out.println("x1 = " + x);
         if (collisionChecker.checkCollision(this, listBarrier)) {
-            moveLeft();
-            System.out.println("x2 = " + x);
+            x-=VELOCITY;
+            isHitWall = true;
+            if (isHitWall) {
+                x-=VELOCITY;
+            }
         }
         ANIMATE --;
         if (Balloon.ANIMATE < 0) {
@@ -34,7 +60,11 @@ public class Balloon extends Entity {
     public void moveLeft() {
         x-=VELOCITY;
         if (collisionChecker.checkCollision(this, listBarrier)) {
-            moveRight();
+            x-=VELOCITY;
+            isHitWall = true;
+            if (isHitWall) {
+                x-=VELOCITY;
+            }
         }
         ANIMATE --;
         if (Balloon.ANIMATE < 0) {
@@ -42,23 +72,80 @@ public class Balloon extends Entity {
         }
     }
 
+    public void moveUp () {
+        y-=VELOCITY;
+        if (collisionChecker.checkCollision(this, listBarrier)) {
+            y+=VELOCITY;
+            isHitWall = true;
+            if (isHitWall) {
+                y+=VELOCITY;
+            }
+        }
+        ANIMATE --;
+        if (Bomber.ANIMATE < 0) {
+            Bomber.ANIMATE = 30;
+        }
+    }
+
+    public void moveDown() {
+        y+=VELOCITY;
+        if (collisionChecker.checkCollision(this, listBarrier)) {
+            y-=VELOCITY;
+            isHitWall = true;
+            if (isHitWall) {
+                y-=VELOCITY;
+            }
+        }
+        ANIMATE --;
+        if (Bomber.ANIMATE < 0) {
+            Bomber.ANIMATE = 30;
+        }
+    }
     Random random = new Random();
-    int randomDirection = random.nextInt(DIRECTIONS);
+    int randomDirection = random.nextInt(4);
 
     @Override
     public void update() {
         for (int i = 0; i < entities.size(); i++) {
             if (entities.get(i) instanceof Balloon) {
-
                 if (randomDirection == 0) {
                     img = Sprite.movingSprite(Sprite.balloom_right1, Sprite.balloom_right2, Sprite.balloom_right3, Balloon.ANIMATE, Balloon.TIME).getFxImage();
                     moveRight();
+                    if (isHitWall) {
+                        List randomList = get(listDirection, 3);
+                        randomDirection = (int) randomList.get(0);
+                        isHitWall = false;
+                    }
+
                 }
+
                 if (randomDirection == 1) {
                     img = Sprite.movingSprite(Sprite.balloom_left1, Sprite.balloom_left2, Sprite.balloom_left3, Balloon.ANIMATE, Balloon.TIME).getFxImage();
                     moveLeft();
+                    if (isHitWall) {
+                        List randomList = get(listDirection, 3);
+                        randomDirection = (int) randomList.get(0);
+                        isHitWall = false;
+                    }
                 }
-//                System.out.println(random);
+                if (randomDirection == 2) {
+                    img = Sprite.movingSprite(Sprite.balloom_left1, Sprite.balloom_left2, Sprite.balloom_left3, Balloon.ANIMATE, Balloon.TIME).getFxImage();
+                    moveUp();
+                    if (isHitWall) {
+                        List randomList = get(listDirection, 3);
+                        randomDirection = (int) randomList.get(0);
+                        isHitWall = false;
+                    }
+                }
+                if (randomDirection == 3) {
+                    img = Sprite.movingSprite(Sprite.balloom_left1, Sprite.balloom_left2, Sprite.balloom_left3, Balloon.ANIMATE, Balloon.TIME).getFxImage();
+                    moveDown();
+                    if (isHitWall) {
+                        List randomList = get(listDirection, 3);
+                        randomDirection = (int) randomList.get(0);
+                        isHitWall = false;
+                    }
+                }
             }
 
         }
--- src/main/java/bomberman/linhtinh/CollisionChecker.java
+++ src/main/java/bomberman/linhtinh/CollisionChecker.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class CollisionChecker {
     private static final int MAXWIDTHBOMBER = 24;
     private static final int MAXHEIGHTBOMBER = 30;
+
     public boolean checkCollision(Entity entity, List<Entity> listBarrier) {
         for (int i = 0; i < listBarrier.size(); i++) {
             if (!(listBarrier.get(i) instanceof Grass)) {
@@ -15,6 +16,50 @@ public class CollisionChecker {
                         && entity.getX() + MAXWIDTHBOMBER > listBarrier.get(i).getX()
                         && entity.getY() < listBarrier.get(i).getY() + Sprite.SCALED_SIZE
                         && entity.getY() + MAXHEIGHTBOMBER > listBarrier.get(i).getY() ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkHitRight(Entity entity, List<Entity> listBarrier, int width) {
+        for (int i = 0; i < listBarrier.size(); i++) {
+            if (!(listBarrier.get(i) instanceof Grass)) {
+                if (entity.getX() + width > listBarrier.get(i).getX()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkHitLeft(Entity entity, List<Entity> listBarrier) {
+        for (int i = 0; i < listBarrier.size(); i++) {
+            if (!(listBarrier.get(i) instanceof Grass)) {
+                if (entity.getX() < listBarrier.get(i).getX() + Sprite.SCALED_SIZE) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkHitUp(Entity entity, List<Entity> listBarrier) {
+        for (int i = 0; i < listBarrier.size(); i++) {
+            if (!(listBarrier.get(i) instanceof Grass)) {
+                if (entity.getY() < listBarrier.get(i).getY() + Sprite.SCALED_SIZE) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkHitDown(Entity entity, List<Entity> listBarrier, int height) {
+        for (int i = 0; i < listBarrier.size(); i++) {
+            if (!(listBarrier.get(i) instanceof Grass)) {
+                if (entity.getY() + height > listBarrier.get(i).getY()) {
                     return true;
                 }
             }
